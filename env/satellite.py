699852e325634d93a3a2a388d4a26e3458ca87cd
SprayWaterXCW <yuzhy2023@shanghaitech.edu.cn>
2024-07-01 19:24:26 +0800

    114514

---
+++ env/satellite.py
@@ -0,0 +1,6 @@
+class Satellite:
+    def __init__(self, CPU_number=4, GPU_number=4, MEMORY_mb=4096, POWER_W=200):
+        self.CPU_number = CPU_number
+        self.GPU_number = GPU_number
+        self.MEMORY_mb = MEMORY_mb
+        self.POWER_W=200
